d8cbcfb4b1af5e474b0c6243dffe6fc383760009
Romain <leconteromain.lr@gmail.com>
2018-02-18 11:40:23 +0100

    add validation for table db required to sav new info

--- src/books.js
+++ src/books.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const BookSchema = new Schema({
-    title:String,
+    title:{type:String, required:[true,'title empty, sav fail']},
     totalPages:{type:Number, default:0}
 });
 const Book = mongoose.model('book', BookSchema);
